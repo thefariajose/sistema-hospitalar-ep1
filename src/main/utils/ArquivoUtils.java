d51041cd6f2c8ea48275fe623681b62687c1698a
José Luiz <joseluiz.13051@gmail.com>
2025-10-05 17:35:13 -0300

    Criado a classe para ler e escrever arquivos csv

---
+++ src/main/utils/ArquivoUtils.java
@@ -0,0 +1,39 @@
+package main.utils;
+
+import java.io.*;
+import java.util.*;
+
+public class ArquivoUtils {
+    public static void salvarEmCSV(String nomeArquivo, List<String> linhas){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))){
+            for(String linha : linhas){
+                writer.write(linha);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar arquivo: " + e.getMessage());
+        }
+    }
+
+
+
+public static List<String> carregarDeCSV(String nomeArquivo) {
+        List<String> linhas = new ArrayList<>();
+        File arquivo = new File(nomeArquivo);
+        if (!arquivo.exists()) {
+            return linhas; // retorna lista vazia se não existir
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                linhas.add(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo: " + e.getMessage());
+        }
+
+        return linhas;
+    }
+}
+
